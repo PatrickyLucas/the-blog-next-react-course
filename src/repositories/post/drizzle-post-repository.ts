ee51fb764f5afa019817964bf5a6714cc91cf2cb
Patricky Lucas <patrickylucas@hotmail.com>
2025-11-12 16:22:38 -0300

    feat: Correção de bug no cache + função de log para separar nossos logs do Next.js

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -1,10 +1,11 @@
 import { drizzleDb } from '@/db/drizzle';
 import { PostModel } from '@/models/post/post-model';
+import { logColor } from '@/utils/log-color';
 import { PostRepository } from './post-repository';
 
 export class DrizzlePostRepository implements PostRepository {
   async findAllPublic(): Promise<PostModel[]> {
-    console.log('\n', 'D findAllPublic', '\n');
+    logColor('findAllPublic', Date.now());
     const posts = await drizzleDb.query.posts.findMany({
       orderBy: (posts, { desc }) => desc(posts.createdAt),
       where: (posts, { eq }) => eq(posts.published, true),
@@ -14,7 +15,7 @@ export class DrizzlePostRepository implements PostRepository {
   }
 
   async findBySlugPublic(slug: string): Promise<PostModel> {
-     console.log('\n', 'D findBySlugPublic', '\n');
+    logColor('findBySlugPublic', Date.now());
     const post = await drizzleDb.query.posts.findFirst({
       where: (posts, { eq, and }) =>
         and(eq(posts.slug, slug), eq(posts.published, true)),
@@ -26,7 +27,7 @@ export class DrizzlePostRepository implements PostRepository {
   }
 
   async findAll(): Promise<PostModel[]> {
-    console.log('\n', 'D findAll', '\n');
+    logColor('findAll', Date.now());
     const posts = await drizzleDb.query.posts.findMany({
       orderBy: (posts, { desc }) => desc(posts.createdAt),
     });
@@ -35,7 +36,7 @@ export class DrizzlePostRepository implements PostRepository {
   }
 
   async findById(id: string): Promise<PostModel> {
-    console.log('\n', 'D findById', '\n');
+    logColor('findById', Date.now());
     const post = await drizzleDb.query.posts.findFirst({
       where: (posts, { eq }) => eq(posts.id, id),
     });
